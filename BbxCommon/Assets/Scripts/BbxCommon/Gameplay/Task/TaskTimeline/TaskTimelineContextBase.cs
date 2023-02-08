1de3c48c48b388607b2953c45a552a3137bb2d4f
MeIsFishes <627935375@qq.com>
2023-02-08 22:15:05 +0800

    小更改

--- BbxCommon/Assets/Scripts/BbxCommon/Gameplay/Task/TaskTimeline/TaskTimelineContextBase.cs
+++ BbxCommon/Assets/Scripts/BbxCommon/Gameplay/Task/TaskTimeline/TaskTimelineContextBase.cs
@@ -2,27 +2,9 @@
 
 namespace BbxCommon
 {
-    public enum EContextType
-    {
-        None,
-        Test,
-        Buff,
-        Action,
-        ActionMix,
-        Projectile,
-    }
-
     public abstract class TaskTimelineContextBase : PooledObject
     {
-        public EContextType Type;
         public GameObject Caster;
         public GameObject Target;
-
-        protected abstract void InitContextType();
-
-        public TaskTimelineContextBase()
-        {
-            InitContextType();
-        }
     }
 }
--- BbxCommon/Assets/Scripts/BbxCommon/RandomFunction.cs
+++ BbxCommon/Assets/Scripts/BbxCommon/RandomFunction.cs
@@ -7,7 +7,7 @@ namespace BbxCommon
         /// <summary>
         /// Input a probability in [0, 1], return if it hits.
         /// </summary>
-        public static bool IsSucceededPercen(float probability)
+        public static bool IsSucceededRatio(float probability)
         {
             var rand = Random.Range(0f, 1f);
             if (rand < probability)
@@ -31,18 +31,18 @@ namespace BbxCommon
         }
 
         /// <summary>
-        /// Input a normal distribution factor, return a random result in range [0f, 100f].
+        /// Input a normal distribution factor, return a random result in range [0f, 1f].
         /// </summary>
         /// <param name="factor"> Normal distribution factor, must be greater than 0.
         /// When it's less than 1, the random result will more likely to be closer to 0,
-        /// and if it's greater than 1, the result will more likely to be closer to 100. </param>
+        /// and if it's greater than 1, the result will more likely to be closer to 1. </param>
         /// <returns> A randomly created result represents the probability in percentage. </returns>
         public static float NormalDistributionProbability(float factor)
         {
             var res = Random.Range(0f, 1f);
             res = Mathf.Pow(res, factor);
             res = 1 - res;
-            return res * 100;
+            return res;
         }
     }
 }
