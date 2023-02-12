9e34b43b3695ce7ea1c580cf24ad8c70f5324210
MeIsFishes <627935375@qq.com>
2023-02-12 15:42:50 +0800

    UiDragable添加不同状态时可以应用不同的样式

--- BbxCommon/Assets/Scripts/BbxCommon/Ui/Misc/UiDragable.cs
+++ BbxCommon/Assets/Scripts/BbxCommon/Ui/Misc/UiDragable.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -11,10 +12,29 @@ namespace BbxCommon.Ui
     /// </summary>
     public class UiDragable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerMoveHandler, IDragHandler, IPointerDownHandler, IPointerUpHandler
     {
+        #region Variables
         // public settings
-        public bool AlwaysCentered = true;
-        [ShowIf("AlwaysCentered")]
+        public bool AlwaysCenter = true;
+        [ShowIf("AlwaysCenter")]
+        [Tooltip("If true, the object move to mouse's position when it is down.")]
         public bool CenterWhenDown = true;
+        [Tooltip("If true, the object will turn back to its start position when mouse is up.")]
+        public bool TurnBackWhenUp;
+
+        [FoldoutGroup("Objects in different state")]
+        [Tooltip("If true, it will activate different objects during different states.")]
+        public bool UseDiffWhenActive;
+        [FoldoutGroup("Objects in different state"), ShowIf("UseDiffWhenActive")]
+        public float ScaleWhenActive;
+        [FoldoutGroup("Objects in different state"), ShowIf("UseDiffWhenActive")]
+        [Tooltip("Objects to be activated when doesn't interact with mouse.")]
+        public List<GameObject> InactiveItems;
+        [FoldoutGroup("Objects in different state"), ShowIf("UseDiffWhenActive")]
+        [Tooltip("Objects to be activated when mouse stays in but nor drag.")]
+        public List<GameObject> StayItems;
+        [FoldoutGroup("Objects in different state"), ShowIf("UseDiffWhenActive")]
+        [Tooltip("Objects to be activated when dragging.")]
+        public List<GameObject> DragItems;
 
         // callbacks
         public UnityAction<PointerEventData> OnPointerEnter;
@@ -34,11 +54,27 @@ namespace BbxCommon.Ui
         private bool m_Dragging;
         private PointerEventData m_CurrentData;
         private Vector3 m_DragOffset;
+        private Vector3 m_StartPos;
+        #endregion
+
+        #region CallbacksAndTick
+        protected void Awake()
+        {
+            if (UseDiffWhenActive)
+                OnStateInactive();
+
+            m_StartPos = transform.position;
+        }
 
         protected void Update()
         {
-            if (m_PointerIn)
+            if (m_PointerIn && !m_Dragging)
+            {
+                if (UseDiffWhenActive)
+                    OnStateStay();
                 OnPointerStay?.Invoke(m_CurrentData);
+            }
+
             if (m_Dragging)
                 DraggedTime += BbxRawTimer.DeltaTime;
         }
@@ -47,6 +83,7 @@ namespace BbxCommon.Ui
         {
             m_PointerIn = true;
             m_CurrentData = eventData;
+
             OnPointerEnter?.Invoke(eventData);
         }
 
@@ -57,18 +94,27 @@ namespace BbxCommon.Ui
 
         void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
         {
+            if (UseDiffWhenActive)
+                OnStateInactive();
+
             m_PointerIn = false;
             m_CurrentData = null;
+
             OnPointerExit?.Invoke(eventData);
+            Debug.Log("ÍË³ö");
         }
 
 
         void IDragHandler.OnDrag(PointerEventData eventData)
         {
-            if (AlwaysCentered)
+            if (UseDiffWhenActive)
+                OnStateDrag();
+
+            if (AlwaysCenter)
                 transform.position = eventData.position;
             else
                 transform.position = eventData.position.AsVector3XY() + m_DragOffset;
+
             OnDrag?.Invoke(eventData);
         }
 
@@ -76,13 +122,57 @@ namespace BbxCommon.Ui
         {
             m_Dragging = true;
             m_DragOffset = transform.position - eventData.position.AsVector3XY();
+
+            if (AlwaysCenter && CenterWhenDown)
+                transform.position = eventData.position.AsVector3XY();
+
             OnPointerDown?.Invoke(eventData);
         }
 
         void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
         {
             m_Dragging = false;
+
+            if (TurnBackWhenUp)
+                transform.position = m_StartPos;
+
             OnPointerUp?.Invoke(eventData);
         }
+        #endregion
+
+        #region InternalFunctions
+        private void OnStateInactive()
+        {
+            foreach (var obj in StayItems)
+                obj.SetActive(false);
+            foreach (var obj in DragItems)
+                obj.SetActive(false);
+            foreach (var obj in InactiveItems)
+                obj.SetActive(true);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        private void OnStateStay()
+        {
+            foreach (var obj in InactiveItems)
+                obj.SetActive(false);
+            foreach (var obj in DragItems)
+                obj.SetActive(false);
+            foreach (var obj in StayItems)
+                obj.SetActive(true);
+            transform.localScale = new Vector3(ScaleWhenActive, ScaleWhenActive, ScaleWhenActive);
+        }
+
+        private void OnStateDrag()
+        {
+            foreach (var obj in InactiveItems)
+                obj.SetActive(false);
+            foreach (var obj in StayItems)
+                obj.SetActive(false);
+            foreach (var obj in DragItems)
+                obj.SetActive(true);
+            transform.localScale = new Vector3(ScaleWhenActive, ScaleWhenActive, ScaleWhenActive);
+        }
+        #endregion
     }
 }
