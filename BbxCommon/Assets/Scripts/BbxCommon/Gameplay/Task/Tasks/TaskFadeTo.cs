cd8e32a471ebc504c144ad971f26f8f8c3bea9b1
MeIsFishes <627935375@qq.com>
2023-04-02 20:54:31 +0800

    上传一个timeline示例

---
+++ BbxCommon/Assets/Scripts/BbxCommon/Gameplay/Task/Tasks/TaskFadeTo.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace BbxCommon
+{
+    public class TaskFadeTo : TaskBase
+    {
+        public GameObject Target;
+        public float FinalAlpha;    // 最终透明度
+        public float Duration;
+
+        private float m_FadeRate;   // 衰减速率
+        private SpriteRenderer m_SpriteComp;
+        private float m_BeginningTime;
+
+        public void Init(GameObject target, float finalAlpha, float duration)
+        {
+            Target = target;
+            FinalAlpha = finalAlpha;
+            Duration = duration;
+        }
+
+        protected override void OnEnter()
+        {
+            Target.TryGetComponent(out m_SpriteComp);
+            if (m_SpriteComp == null) return;
+            m_FadeRate = (FinalAlpha - m_SpriteComp.color.a) / Duration;
+            m_BeginningTime = Time.time;
+        }
+
+        protected override ETaskState OnExecute()
+        {
+            if (m_SpriteComp == null) return ETaskState.Finished;
+            if (Time.time >= m_BeginningTime + Duration)
+            {
+                m_SpriteComp.color = new Color(
+                m_SpriteComp.color.r,
+                m_SpriteComp.color.g,
+                m_SpriteComp.color.b,
+                FinalAlpha);
+                return ETaskState.Finished;
+            }
+            m_SpriteComp.color = new Color(
+                m_SpriteComp.color.r,
+                m_SpriteComp.color.g,
+                m_SpriteComp.color.b,
+                m_SpriteComp.color.a + m_FadeRate * Time.deltaTime);
+            return ETaskState.Running;
+        }
+
+        protected override void OnExit()
+        {
+            
+        }
+    }
+}
---
+++ BbxCommon/Assets/Scripts/BbxCommon/Gameplay/Task/Tasks/TaskMoveTo.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace BbxCommon
+{
+    public class TaskMoveTo : TaskBase
+    {
+        public GameObject Target { get; private set; }
+        public Vector3 TargetPosition { get; private set; }
+        public float BeginningSpeed { get; private set; }
+        public float Acceleration { get; private set; }
+
+        private Vector3 m_Direction;
+        private float m_Speed;
+
+        public void Init(GameObject target, Vector3 targetPosition, float beginningSpeed, float acceleration = 0)
+        {
+            Target = target;
+            TargetPosition = targetPosition;
+            BeginningSpeed = beginningSpeed;
+            Acceleration = acceleration;
+        }
+
+        protected override void OnEnter()
+        {
+            m_Direction = (TargetPosition - Target.transform.position).normalized;
+            m_Speed = BeginningSpeed;
+        }
+
+        protected override ETaskState OnExecute()
+        {
+            var deltaDistance = m_Speed * Time.deltaTime;
+            // 因为距离计算涉及到开方，性能会相对差一些，所以这里可以直接用四则运算和比较完成
+            if (Mathf.Abs(TargetPosition.x - Target.transform.position.x) <= Mathf.Abs(m_Direction.x * deltaDistance) &&
+                Mathf.Abs(TargetPosition.y - Target.transform.position.y) <= Mathf.Abs(m_Direction.y * deltaDistance))
+            {
+                Target.transform.position = TargetPosition;
+                return ETaskState.Finished;
+            }
+            else
+            {
+                Target.transform.position += m_Direction * deltaDistance;
+                m_Speed += Acceleration * Time.deltaTime;
+                return ETaskState.Running;
+            }
+        }
+
+        protected override void OnExit()
+        {
+            
+        }
+    }
+}
