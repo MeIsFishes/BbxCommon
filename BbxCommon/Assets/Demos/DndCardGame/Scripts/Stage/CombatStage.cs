131a83bbe7c9d7954720323c945786cf76161e13
NaughtyGandum <37438978+BernieGriffith@users.noreply.github.com>
2024-07-31 21:58:08 +0800

    Update CombatStage.cs
    修复编译

--- BbxCommon/Assets/Demos/DndCardGame/Scripts/Stage/CombatStage.cs
+++ BbxCommon/Assets/Demos/DndCardGame/Scripts/Stage/CombatStage.cs
@@ -4,7 +4,6 @@ using BbxCommon;
 using BbxCommon.Ui;
 using Dcg.Ui;
 using System.Collections.Generic;
-
 namespace Dcg
 {
     public class CombatStage
@@ -12,14 +11,10 @@ namespace Dcg
         public static GameStage CreateStage()
         {
             var stage = DcgGameEngine.Instance.StageWrapper.CreateStage("Combat Stage");
-
             stage.SetUiScene(DcgGameEngine.Instance.UiScene, Resources.Load<UiSceneAsset>("DndCardGame/Config/UiScene/UiCombatScene"));
-
             stage.AddLoadItem(new CombatStageInitPlayerData());
             stage.AddLoadItem(new CombatStageInitCombatInfo());
-
             stage.AddLateLoadItem(new CombatStageBindUi());
-
             stage.AddUpdateSystem<CauseDamageSystem>();
             stage.AddUpdateSystem<TakeDamageSystem>();
             stage.AddUpdateSystem<CombatRoundSystem>();
@@ -29,19 +24,20 @@ namespace Dcg
 
             stage.AddStageListener<CombatWinListener>();
             stage.AddStageListener<CombatDefeatedListener>();
+            stage.AddStageListener<CombatHpSyncListener>();
 
             return stage;
         }
 
         private class CombatStageInitPlayerData : IStageLoad
         {
-            private Entity combatEntity;
             void IStageLoad.Load(GameStage stage)
             {
                 var playerComp = EcsApi.GetSingletonRawComponent<LocalPlayerSingletonRawComponent>();
-                combatEntity = EntityCreator.CreateCombatEntity();
-                ConvertCharacterToCombatEntity(playerComp.Characters[0],combatEntity);
-                playerComp.DestroyCharacterEntities();
+                var combatEntity = EntityCreator.CreateCombatEntity();
+                DungeonEntityToCombatEntity(playerComp.DungeonEntities[0], combatEntity);
+                playerComp.DungeonEntities[0].GetGameObject().SetActive(false);
+                playerComp.AddCombatCharacter(combatEntity);
                 var combatInfoComp = EcsApi.AddSingletonRawComponent<CombatInfoSingletonRawComponent>();
                 combatInfoComp.Character = combatEntity;
             }
@@ -49,77 +45,53 @@ namespace Dcg
             void IStageLoad.Unload(GameStage stage)
             {
                 var playerComp = EcsApi.GetSingletonRawComponent<LocalPlayerSingletonRawComponent>();
-                var character = EntityCreator.CreateCharacterEntity();
-                ConvertCombatEntityToCharacter(combatEntity, character);
-                playerComp.AddCharacter(character);
-                DestroyCombatEntity(combatEntity);
+                playerComp.DungeonEntities[0].GetGameObject().SetActive(true);
+                DestroyCombatEntity(playerComp.CombatEntities[0]);
+                playerComp.CombatEntities.Clear();
             }
 
-            void ConvertCharacterToCombatEntity(Entity character,Entity combatEntity)
+            private void DungeonEntityToCombatEntity(Entity dungeonEntity, Entity combatEntity)
             {
-                var charObj=character.GetGameObject();
-                var comBatObj= combatEntity.GetGameObject();
-                comBatObj.transform.localPosition = charObj.transform.localPosition;
-                // ¥¥Ω®≥ı ºø®◊È
+                var dungeonObj = dungeonEntity.GetGameObject();
+                var combatObj = combatEntity.GetGameObject();
+                combatObj.transform.localPosition = dungeonObj.transform.localPosition;
+                // ÂàõÂª∫ÂàùÂßãÂç°ÁªÑ
                 var combatDeckComp = combatEntity.GetRawComponent<CombatDeckRawComponent>();
-                var characterDeckComp = character.GetRawComponent<CharacterDeckRawComponent>();
+                var characterDeckComp = dungeonEntity.GetRawComponent<CharacterDeckRawComponent>();
                 combatDeckComp.DicesInDeck.Clear();
                 foreach (var dice in characterDeckComp.Dices)
                 {
                     combatDeckComp.DicesInDeck.Add(Dice.Create(dice.DiceType));
                 }
                 combatDeckComp.DicesInDeck.Shuffle();
+                // ÂêåÊ≠•Ë°ÄÈáè
+                var combatAttrComp = combatEntity.GetRawComponent<AttributesRawComponent>();
+                var dungeonAttrComp = dungeonEntity.GetRawComponent<AttributesRawComponent>();
+                combatAttrComp.CurHp = dungeonAttrComp.CurHp;
             }
-
-            void ConvertCombatEntityToCharacter(Entity combatEntity,Entity character)
+            private void DestroyCombatEntity(Entity combatEntity)
             {
-                var charObj = character.GetGameObject();
-                var comBatObj = combatEntity.GetGameObject();
-                charObj.transform.localPosition = comBatObj.transform.localPosition;
-                // ¥¥Ω®≥ı ºø®◊È
-                var combatDeckComp = combatEntity.GetRawComponent<CombatDeckRawComponent>();
-                var characterDeckComp = character.GetRawComponent<CharacterDeckRawComponent>();
-                characterDeckComp.Dices.Clear();
-                foreach (var dice in combatDeckComp.DicesInDeck)
-                {
-                    characterDeckComp.Dices.Add(Dice.Create(dice.DiceType));
-                }
-                characterDeckComp.Dices.Shuffle();
-            }
-
-            void DestroyCombatEntity(Entity combatEntity)
-            {
-                if (combatEntity!=null)
-                {
-                    var combatDeckComp = combatEntity.GetRawComponent<CombatDeckRawComponent>();
-                    combatDeckComp.DicesInDeck = null;
-                    combatEntity.UnbindHud<HudCharacterStatusController>();
-                    combatEntity.UnbindHud<HudDamageTweenTipController>();
-                    combatEntity.Destroy();
-                }
+                combatEntity.Destroy();
             }
         }
 
         private class CombatStageInitCombatInfo : IStageLoad
         {
             private Entity m_MonsterEntity;
-
             void IStageLoad.Load(GameStage stage)
             {
-                // ¥¥Ω®π÷ŒÔ
+                // ÂàõÂª∫ÊÄ™Áâ©
                 var dungeonRoomComp = EcsApi.GetSingletonRawComponent<DungeonRoomSingletonRawComponent>();
                 var roomData = DataApi.GetData<RoomData>();
                 var roomPos = dungeonRoomComp.CurRoom.GetGameObject().transform.position;
                 var spawnPos = roomPos + roomData.MonsterOffset;
                 m_MonsterEntity = EntityCreator.CreateMonsterEntity(GameUtility.RandomPool.GetRandomMonster(), spawnPos, Quaternion.LookRotation((roomPos - spawnPos).SetY(0)));
-
-                // ≥ı ºªØ’Ω≥°–≈œ¢
+                // ÂàùÂßãÂåñÊàòÂú∫‰ø°ÊÅØ
                 var combatInfoComp = EcsApi.GetSingletonRawComponent<CombatInfoSingletonRawComponent>();
                 var combatRoundComp = EcsApi.AddSingletonRawComponent<CombatRoundSingletonRawComponent>();
                 combatInfoComp.Monster = m_MonsterEntity;
                 combatRoundComp.EnterCombat = true;
             }
-
             void IStageLoad.Unload(GameStage stage)
             {
                 EcsApi.RemoveSingletonRawComponent<CombatInfoSingletonRawComponent>();
@@ -127,7 +99,6 @@ namespace Dcg
                 m_MonsterEntity.Destroy();
             }
         }
-
         private class CombatStageBindUi : IStageLoad
         {
             void IStageLoad.Load(GameStage stage)
@@ -136,10 +107,8 @@ namespace Dcg
                 UiApi.GetUiController<UiCharacterInfoController>().Bind(combatInfoComp.Character);
                 UiApi.GetUiController<UiMonsterInfoController>().Bind(combatInfoComp.Monster);
             }
-
             void IStageLoad.Unload(GameStage stage)
             {
-
             }
         }
     }
