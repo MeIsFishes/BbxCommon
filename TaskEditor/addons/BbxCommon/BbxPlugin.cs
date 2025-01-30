70c4b5f42d2053c18badc643b5a2107969efaa67
MeIsFishes <627935375@qq.com>
2025-01-30 18:40:19 +0800

    强制上传被忽略的godot文件

--- TaskEditor/addons/BbxCommon/BbxPlugin.cs
+++ TaskEditor/addons/BbxCommon/BbxPlugin.cs
@@ -10,8 +10,8 @@ namespace BbxCommon
 		public override void _EnterTree()
 		{
 			var script = GD.Load<Script>("res://addons/BbxCommon/BbxButton.cs");
-            AddCustomType("BbxButton", "Button", script, null);
-        }
+			AddCustomType("BbxButton", "Button", script, null);
+		}
 
 		public override void _ExitTree()
 		{
