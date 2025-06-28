6755cc5b5a1c20d18056e280669c7482d543429a
MeIsFishes <627935375@qq.com>
2025-06-28 17:26:37 +0800

    上传TaskConnectPoint

---
+++ BbxCommon/Assets/Scripts/BbxCommon/GameFramework/Task/TaskDrive/TaskConnectPoint.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BbxCommon.Internal
+{
+    public enum ETaskConnectPointType
+    {
+        Single,
+        Multiple,
+    }
+
+    public class TaskConnectPoint : MonoBehaviour
+    {
+        public ETaskConnectPointType ConnectPointType = ETaskConnectPointType.Multiple;
+        public List<TaskBase> Tasks = new();
+    }
+}
