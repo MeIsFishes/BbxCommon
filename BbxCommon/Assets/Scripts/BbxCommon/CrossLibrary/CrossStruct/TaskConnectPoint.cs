99e6fd16bd108399db185fa8d8214589da4357c8
MeIsFishes <627935375@qq.com>
2025-08-02 00:40:04 +0800

    将TaskConnectPoint挪到CrossLibrary

---
+++ BbxCommon/Assets/Scripts/BbxCommon/CrossLibrary/CrossStruct/TaskConnectPoint.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BbxCommon.Internal
+{
+    public enum ETaskConnectPointType
+    {
+        Single,
+        Multiple,
+    }
+
+    public class TaskConnectPoint
+    {
+        public ETaskConnectPointType ConnectPointType = ETaskConnectPointType.Multiple;
+        public List<TaskBase> Tasks = new();
+        internal List<int> TaskRefrenceIds = new();
+    }
+}
